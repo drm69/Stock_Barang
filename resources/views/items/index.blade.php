48dddd6135d796e8e95cfeb8bb39f2910123291a
drm69 <yukihira14darma@gmail.com>
2025-02-21 22:24:01 +0700

    update

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -12,19 +12,28 @@
     @endif
 
     <a href="{{route('items.create')}}">Tambahkan Barang</a>
-
-    <ul>
+    <table border="1">
+        <tr>
+            <td>No.</td>
+            <td>Nama Barang</td>
+            <td>Jumlah</td>
+            <td>Deskripsi</td>
+        </tr>
         @foreach ($items as $item)
-            <li>
-                {{$item->name}} -
-                <a href="{{ route('items.edit', $item)}}">Edit</a>
-                <form action="{{ route('items.destroy', $item)}}" method="POST" style="display:inline">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Delete</button>
-                </form>
-            </li>
-        @endforeach
+        <tr>
+            <form action="{{ route('items.destroy', $item)}}" method="POST" style="display:inline">
+                @csrf
+                <td>{{$item->id}} </td>
+                <td>{{$item->Nama_Barang}} </td>
+                <td>{{$item ->Jumlah}}</td>
+                <td>{{$item ->Deskripsi}}</td>
+                <td><a href="{{ route('items.edit', $item)}}">Edit</a></td>
+               <td> @method('DELETE') <button type="submit">Delete</button></td>
+            </form>
+            @endforeach
+        </tr>
+    </table>
+    <ul>
     </ul>
 </body>
 </html>
