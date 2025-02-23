5511c6370e0cb4daa0fd4220aa372995fd40378b
drm69 <yukihira14darma@gmail.com>
2025-02-23 12:00:49 +0700

    update tampilan

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -8,32 +8,36 @@
 <body>
     <h1>Barang</h1>
     @if (session('success'))
-        <p>{{session('success')}}</p>
+        <p>{{ session('success') }}</p>
     @endif
 
-    <a href="{{route('items.create')}}">Tambahkan Barang</a>
+    <a href="{{ route('items.create') }}">Tambahkan Barang</a>
     <table border="1">
         <tr>
             <td>No.</td>
             <td>Nama Barang</td>
             <td>Jumlah</td>
             <td>Deskripsi</td>
+            <td>Aksi</td>
         </tr>
+        @php $no = 1; 
+        @endphp
         @foreach ($items as $item)
         <tr>
-            <form action="{{ route('items.destroy', $item)}}" method="POST" style="display:inline">
+            <form action="{{ route('items.destroy', $item) }}" method="POST" style="display:inline">
                 @csrf
-                <td>{{$item->id}} </td>
-                <td>{{$item->Nama_Barang}} </td>
-                <td>{{$item ->Jumlah}}</td>
-                <td>{{$item ->Deskripsi}}</td>
-                <td><a href="{{ route('items.edit', $item)}}">Edit</a></td>
-               <td> @method('DELETE') <button type="submit">Delete</button></td>
+                @method('DELETE')
+                <td>{{ $no++ }}</td>
+                <td>{{ $item->Nama_Barang }}</td>
+                <td>{{ $item->Jumlah }}</td>
+                <td>{{ $item->Deskripsi }}</td>
+                <td>
+                    <a href="{{ route('items.edit', $item) }}">Edit</a>
+                    <button type="submit">Delete</button>
+                </td>
             </form>
-            @endforeach
         </tr>
+        @endforeach
     </table>
-    <ul>
-    </ul>
 </body>
 </html>
